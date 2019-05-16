4b59f8778e28a5b415e175a52352c7309d08d718
Radford98 <powelbra@oregonstate.edu>
2019-05-16 15:07:34 -0700

    Parse function separated from main

--- smallsh.c
+++ smallsh.c
@@ -14,15 +14,15 @@
 #include <sys/types.h>
 #include <signal.h>
 
+int ParseArgs(char**, char*);	// Accepts array for arguments and string to parse; Returns number of arguments
+
 void main() {
 	// Variables for getting user input
 	int numEnt = -5;	// Number of characters entered
 	size_t buffer = 0;	// Size of allocated buffer
 	char* command = NULL;	// Entered string + \n + \0 
 
-	char tokenize[2049];	// Copies the command for tokenization, maximum of 2048 chars + \n
 	char* argArr[512];	// Array for arguments, maximum number of arguments is 512
-	char delim[] = " ";	// Deliminater for strtok
 	int numArgs;		// Holds the number of arguments
 
 while(1) {	// Keep asking for commands until exit
@@ -31,18 +31,17 @@ while(1) {	// Keep asking for commands until exit
 	fflush(stdout);
 	
 	numEnt = getline(&command, &buffer, stdin);
-	if (numEnt == 1)	// Ignore empty command
+	// Ignore empty or commented lines
+	if (numEnt == 1 || command[0] == '#')
 		continue;
 	// Remove newline from command
 	command[strcspn(command, "\n")] = '\0';
-	strcpy(tokenize, command);
-	
-	// Grab first argument
-	argArr[0] = strtok(tokenize, delim);
+
+	// Parse commandline
+	numArgs = ParseArgs(argArr, command);
+
 	// Check for built in commands
-	if (command[0] == '#') {//(strcmp(command[0], "#") == 0) {
-		// Ignore the line
-	} else if (strcmp(argArr[0], "exit") == 0) {
+	if (strcmp(argArr[0], "exit") == 0) {
 		// Implement exit (kill processes)
 		return;
 	} else if (strcmp(argArr[0], "cd") == 0) {
@@ -50,39 +49,36 @@ while(1) {	// Keep asking for commands until exit
 	} else if (strcmp(argArr[0], "status") == 0) {
 		// Implement status
 	} else { // Run whatever command came through
-		// Parse the given command
-		numArgs = 1;	// Index for array
-		argArr[numArgs] = strtok(NULL, delim); // Pull next argument
-		// Loop over argument list until there are no arguments or redirection/background is detected
-		while (argArr[numArgs] != NULL
-				&& strcmp(argArr[numArgs], "<") != 0
-				&& strcmp(argArr[numArgs], ">") != 0
-				&& strcmp(argArr[numArgs], "&") != 0) {
-			numArgs++;			// Increment array index
-			argArr[numArgs] = strtok(NULL, delim);
-		}
-
 		for (int i = 0; i < numArgs; i++) {
 			printf("Argument %d: %s\n", i, argArr[i]);
 		}
 		printf("Total arguments: %d\n", numArgs);
 
 	}
-	/* Playing with strtok
-	char delim[] = " ";
-	char* array[3];
-	array[0] = strtok(command, delim);
-	array[1] = strtok(NULL, delim);
-	array[2] = strtok(NULL, delim);
-	printf("Tokens: '%s', '%s', '%s'\n", array[0], array[1], array[2]);
-
-	int size = strcspn(orig, "a");
-	printf("First 'a' of original string is at index %d\n", size);
-	printf("Should be 'a': %c\n", orig[size]);
-	*/
 
 	free(command);
 	command = NULL;
 }
 	return;
+}
+
+
+int ParseArgs(char** array, char* parseMe) {
+	int args = 0;		// Array index and number of arguments
+	char tokenize[2048];	// Copies the command for tokenization, maximum of 2048 chars
+	char delim[] = " ";	// Deliminater
+	
+	strcpy(tokenize, parseMe);
+	array[args] = strtok(tokenize, delim);
+
+	// Loop over argument list until there are no arguments left or redirection/background is detected
+	while (array[args] != NULL
+			&& strcmp(array[args], "<") != 0
+			&& strcmp(array[args], ">") != 0
+			&& strcmp(array[args], "&") != 0) {
+		args++;			// Increment array index
+		array[args] = strtok(NULL, delim);
+	}
+
+	return args;
 }
